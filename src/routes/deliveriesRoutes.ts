0fbaa3656698693df452395d57fe20e9a213453f
Gabrielgamas <gamas.enge@hotmail.com>
2025-08-04 09:55:42 -0300

    listando entregas

--- src/routes/deliveriesRoutes.ts
+++ src/routes/deliveriesRoutes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { DeliveriesController } from "@/controllers/deliveriesController";
 import { ensureAuthenticated } from "@/middlewares/ensureAuthenticated";
+import { verifyUserAuthorization } from "@/middlewares/verifyUserAuthorization";
 
 const deliveriesRoutes = Router();
 
 const deliveriesController = new DeliveriesController();
 
-deliveriesRoutes.use(ensureAuthenticated);
+deliveriesRoutes.use(ensureAuthenticated, verifyUserAuthorization(["sale"]));
 deliveriesRoutes.post("/", deliveriesController.create);
+deliveriesRoutes.get("/", deliveriesController.index);
 
 export { deliveriesRoutes };
