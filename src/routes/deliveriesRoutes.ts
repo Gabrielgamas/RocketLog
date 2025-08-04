ab76ca670e794a4aee7d877ce02acd583bc75a9c
Gabrielgamas <gamas.enge@hotmail.com>
2025-08-04 10:23:07 -0300

    criando controller, rotas dos logs das entregas

--- src/routes/deliveriesRoutes.ts
+++ src/routes/deliveriesRoutes.ts
@@ -2,13 +2,17 @@ import { Router } from "express";
 import { DeliveriesController } from "@/controllers/deliveriesController";
 import { ensureAuthenticated } from "@/middlewares/ensureAuthenticated";
 import { verifyUserAuthorization } from "@/middlewares/verifyUserAuthorization";
+import { DeliveriesStatusController } from "@/controllers/deliveriesStatusController";
 
 const deliveriesRoutes = Router();
 
 const deliveriesController = new DeliveriesController();
+const deliveriesStatusController = new DeliveriesStatusController();
 
 deliveriesRoutes.use(ensureAuthenticated, verifyUserAuthorization(["sale"]));
 deliveriesRoutes.post("/", deliveriesController.create);
 deliveriesRoutes.get("/", deliveriesController.index);
+
+deliveriesRoutes.patch("/:id/status", deliveriesStatusController.update);
 
 export { deliveriesRoutes };
