1978fa1547002786e38c3750c07f029a31716042
Gabrielgamas <gamas.enge@hotmail.com>
2025-08-04 16:24:19 -0300

    Teste de cadastrando usuario

---
+++ src/tests/users-controller.test.ts
@@ -0,0 +1,24 @@
+import request from "supertest";
+import { app } from "@/app";
+import { prisma } from "@/database/prisma";
+
+describe("UsersController", () => {
+  let user_id: string;
+
+  afterAll(async () => {
+    await prisma.user.delete({ where: { id: user_id } });
+  });
+
+  it("should create a new user successfully", async () => {
+    const response = await request(app).post("/users").send({
+      name: "Test User",
+      email: "testeuser@example.com",
+      password: "password123",
+    });
+    expect(response.status).toBe(201);
+    expect(response.body).toHaveProperty("id");
+    expect(response.body.name).toBe("Test User");
+
+    user_id = response.body.id;
+  });
+});
