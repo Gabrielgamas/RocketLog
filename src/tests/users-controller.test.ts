70af55bee661cef74b30dda5a8a274b8963003df
Gabrielgamas <gamas.enge@hotmail.com>
2025-08-04 16:29:59 -0300

    Testando email

--- src/tests/users-controller.test.ts
+++ src/tests/users-controller.test.ts
@@ -21,4 +21,24 @@ describe("UsersController", () => {
 
     user_id = response.body.id;
   });
+
+  it("should throw an error if user with same email already exists", async () => {
+    const response = await request(app).post("/users").send({
+      name: "Duplicate User",
+      email: "testeuser@example.com",
+      password: "password123",
+    });
+    expect(response.status).toBe(400);
+    expect(response.body.message).toBe("User with same email already exists");
+  });
+
+  it("should throw avalidation error if email is invalid", async () => {
+    const response = await request(app).post("/users").send({
+      name: "Test User",
+      email: "Invalid-email",
+      password: "password123",
+    });
+    expect(response.status).toBe(400);
+    expect(response.body.message).toBe("validation error");
+  });
 });
